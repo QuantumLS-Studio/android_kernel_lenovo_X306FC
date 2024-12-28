98483e9ebe38f321b96593c392116a78eae494d4
FTS427 <FTS427@outlook.com>
2024-12-28 15:32:36 +0800

    awa

--- compile_kernel.sh
+++ compile_kernel.sh
@@ -5,6 +5,8 @@
 
 export LC_ALL=C
 export ARCH=arm64
+export KBUILD_BUILD_HOST=Arch-Linux
+export KBUILD_BUILD_USER=FTS427
 
 # Get the absolute paths for CLANG_PATH and GCC_PATH
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
@@ -14,11 +16,11 @@ GCC_PATH="${SCRIPT_DIR}/../prebuilt/gcc-64/"
 
 # Clone Toolchains if they don't exist
 if [ ! -d "$CLANG_PATH" ]; then
-    git clone --depth=1 git@github.com:jmpfbmx/android_prebuilts_clang_kernel_linux-x86_clang-4691093.git "$CLANG_PATH"
+    git clone --depth=1 https://gh.llkk.cc/https://github.com/jmpfbmx/android_prebuilts_clang_kernel_linux-x86_clang-4691093.git "$CLANG_PATH"
 fi
 
 if [ ! -d "$GCC_PATH" ]; then
-    git clone --depth=1 git@github.com:jmpfbmx/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git "$GCC_PATH"
+    git clone --depth=1 https://gh.llkk.cc/https://github.com/jmpfbmx/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git "$GCC_PATH"
 fi
 
 # Clean previous build and create output directory
@@ -28,11 +30,19 @@ if [ -d "../KERNEL_OUT" ]; then
 fi
 
 # Build the kernel
-make O="$KERNEL_OUT" ARCH=arm64 amar_row_lte_defconfig
+#make O="$KERNEL_OUT" ARCH=arm64 
 
 PATH="${CLANG_PATH}/bin:${PATH}:${GCC_PATH}/bin:${PATH}" \
 make -j$(nproc --all) O="$KERNEL_OUT" \
                       ARCH=arm64 \
-                      CC="clang" \
+                      CC="ccache clang" \
+                      CLANG_TRIPLE=aarch64-linux-gnu- \
+                      CROSS_COMPILE="${GCC_PATH}/bin/aarch64-linux-android-" \
+                      amar_row_lte_defconfig
+
+PATH="${CLANG_PATH}/bin:${PATH}:${GCC_PATH}/bin:${PATH}" \
+make -j$(nproc --all) O="$KERNEL_OUT" \
+                      ARCH=arm64 \
+                      CC="ccache clang" \
                       CLANG_TRIPLE=aarch64-linux-gnu- \
                       CROSS_COMPILE="${GCC_PATH}/bin/aarch64-linux-android-"
